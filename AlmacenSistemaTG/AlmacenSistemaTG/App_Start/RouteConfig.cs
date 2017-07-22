b721e093d215f0da16f0546577e91ec81b6ad5ac
Pedro Rendón Gómez <rendongope@gmail.com>
2017-07-22 12:30:33 -0500

    PrimerCommit
    Versión con Almacenes

--- AlmacenSistemaTG/AlmacenSistemaTG/App_Start/RouteConfig.cs
+++ AlmacenSistemaTG/AlmacenSistemaTG/App_Start/RouteConfig.cs
@@ -13,7 +13,8 @@ namespace AlmacenSistemaTG
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapRoute
+            (
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ AlmacenSistemaTG/AlmacenSistemaTG/Controllers/AlmacenesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AlmacenSistemaTG.Models;
+
+namespace AlmacenSistemaTG.Controllers
+{
+    public class AlmacenesController : Controller
+    {
+        private SistemaAlmacenEntities db = new SistemaAlmacenEntities();
+
+        // GET: Almacenes
+        public ActionResult Index()
+        {
+            return View(db.Almacen.ToList());
+        }
+
+        // GET: Almacenes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacen.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacenes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Almacenes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdAlmacen,Descripcion,Clave")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Almacen.Add(almacen);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(almacen);
+        }
+
+        // GET: Almacenes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacen.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacenes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdAlmacen,Descripcion,Clave")] Almacen almacen)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(almacen).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(almacen);
+        }
+
+        // GET: Almacenes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Almacen almacen = db.Almacen.Find(id);
+            if (almacen == null)
+            {
+                return HttpNotFound();
+            }
+            return View(almacen);
+        }
+
+        // POST: Almacenes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Almacen almacen = db.Almacen.Find(id);
+            db.Almacen.Remove(almacen);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
